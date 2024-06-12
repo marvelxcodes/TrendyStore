a4f963231173fdedbdec4df6bfbfde834496369c
marvelxcodes <ramakrishnan0046@gmail.com>
2024-06-12 14:19:01 +0530

    Implemented Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from '@/components/Header';
+
+import HomePage from '@/pages/home';
+import CartPage from '@/pages/cart';
+import ProductPage from '@/pages/product';
+import ProductsPage from '@/pages/products';
 
 function App() {
 	return (
-		<main>
+		<BrowserRouter>
 			<Header />
-		</main>
+			<Routes>
+				<Route
+					path='/'
+					element={<HomePage />}
+				/>
+				<Route
+					path='/:category'
+					element={<ProductsPage />}
+				/>
+				<Route
+					path='/product/:id'
+					element={<ProductPage />}
+				/>
+				<Route
+					path='/cart'
+					element={<CartPage />}
+				/>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
