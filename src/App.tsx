10d6d1707b29e2fbf164d299434d861c46341b2d
marvelxcodes <ramakrishnan0046@gmail.com>
2024-06-14 23:39:03 +0530

    Implemented Lazy Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 
+import { store } from '@/store';
 import Header from '@/components/Header';
 
-import HomePage from '@/pages/home';
-import CartPage from '@/pages/cart';
-import ProductPage from '@/pages/product';
-import ProductsPage from '@/pages/products';
+const HomePage = lazy(() => import('@/pages/home'));
+const CartPage = lazy(() => import('@/pages/cart'));
+const ProductPage = lazy(() => import('@/pages/product'));
+const ProductsPage = lazy(() => import('@/pages/products'));
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route
-					path='/'
-					element={<HomePage />}
-				/>
-				<Route
-					path='/:category'
-					element={<ProductsPage />}
-				/>
-				<Route
-					path='/product/:id'
-					element={<ProductPage />}
-				/>
-				<Route
-					path='/cart'
-					element={<CartPage />}
-				/>
-			</Routes>
+			<ReduxProvider store={store}>
+				<Header />
+				<Suspense>
+					<Routes>
+						<Route
+							path='/'
+							element={<HomePage />}
+						/>
+						<Route
+							path='/:category'
+							element={<ProductsPage />}
+						/>
+						<Route
+							path='/product/:id'
+							element={<ProductPage />}
+						/>
+						<Route
+							path='/cart'
+							element={<CartPage />}
+						/>
+					</Routes>
+				</Suspense>
+			</ReduxProvider>
 		</BrowserRouter>
 	);
 }
